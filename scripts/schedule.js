c0b0e5604b40f24bb632c58760683c347ecd0da3
Oliver Brodersen <brodersen88@gmail.com>
2019-12-11 16:36:09 +0100

    Import from Java software

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -3,11 +3,15 @@
 const keywords = [];
 let parameters = false;
 let dateFrom, dateTo, semester;
+const difference = 110;
+
 //Create classes and keywords from loaded array
 if (classes.length !== 0) {
     for (let i = 0; i < classes.length; i++) {
+        const str = classes[i].classname.split("-");
+        const shortName = str[1].slice(0,3);
         //For each item in classes array createCard
-        createCard(i, classes[i].classname, classes[i].nameShort, classes[i].examType, classes[i].dateStart, classes[i].dateEnd, classes[i].color, true);
+        createCard(i, classes[i].classname, shortName , classes[i].examType, classes[i].roomName, classes[i].teacher, classes[i].hasHDMI, classes[i].hasVGA , classes[i].dateStart, classes[i].dateEnd, classes[i].color, true);
     }
 }
 //Bind enter key to run function from input
@@ -17,24 +21,29 @@ document.getElementById('subjectInput').onkeypress = function(e) {
     }
 };
 //Create card
-function createCard(i, classname, nameShort, examType, dateStart, dateEnd, color, init) {
+function createCard(i, classname, nameShort, examType, roomName, teacher, hasHDMI, hasVGA, dateStart, dateEnd, color, init) {
     //Add to context a card with correct information
     if(document.getElementById("class" + i) === null) {
-        const dateStartPart = dateStart.split("-");
-        const dateEndPart = dateEnd.split("-");
-        $( "#content" ).append("<div class='card card" + classname + "' id='class" + i + "' style='border-color: #" + color + ";'><h1>" + classname + "</h1><h3>" + examType + "</h3><h2>" + dateStartPart[1] + "/" + dateStartPart[2] + " - " + dateEndPart[1] + "/" + dateEndPart[2] + "</h2></div>");
+        const dateStartPart = dateStart.split("/");
+        const dateEndPart = dateEnd.split("/");
+        $( "#content" ).append("<div class='card card" + classname + "' id='class" + i + "' style='border-color: " + color + ";'><h3>" + examType + "</h3><h1>" + classname + "</h1><h3>" + teacher + "</h3><h3 class='room'>" + roomName + "</h3><h2>" + dateStartPart[0] + "/" + dateStartPart[1] + " - " + dateEndPart[0] + "/" + dateEndPart[1] + "</h2></div>");
         if (init)
             createKeyword(i, nameShort, color, true);
+        if (hasHDMI === "true")
+            $(".card" + classname).addClass("hasHDMI");
+        if (hasVGA === "true")
+            $(".card" + classname).addClass("hasVGA");
     }
 }
 
 //Creates keyword
 function createKeyword(i, name, color, add) {
     if (document.querySelector('.' + name) == null) {
-        if(add)
+        if(add){
             keywords.push({name: name, color: color});
+        }
         //\u0022 is unicode for "
-        $( "#keywords" ).append("<div class='keyword " + name + "' id='key" + i + "' style='background-color: #" + color + ";'>" + name + "<div class='x' onclick='removeKeyword(\u0022" + name + "\u0022)'>&#x2715;</div></div>");
+        $( "#keywords" ).append("<div class='keyword " + name + "' id='key" + i + "' style='background-color: " + color + ";'>" + name + "<div class='x' onclick='removeKeyword(\u0022" + name + "\u0022)'>&#x2715;</div></div>");
     }
 }
 //Removes keyword from everywhere
@@ -58,29 +67,59 @@ function updateKeywords(){
         //Check if the keyword is finding stuff
         for(let j = 0; j < classes.length; j++){
             const classPart = classes[j].classname.split("-");
-            if(keywords[i].name === classes[j].nameShort || keywords[i].name === classPart[0] || keywords[i].name === classPart[3] || keywords[i].name === classPart[2] || keywords[i].name === classes[j].classname || keywords[i].name === classes[j].examType && document.querySelector('.' + classes[j].classname) == null){
-                //Create card
-                createCard(j,classes[j].classname,classes[j].nameShort,classes[j].examType,classes[j].dateStart,classes[j].dateEnd,classes[j].color, false);
-                //Create keyword tag
-                createKeyword(i, keywords[i].name, classes[j].color, false);
+            //Create keyword tag
+            createKeyword(i, keywords[i].name, keywords[i].color, false);
+            switch (keywords[i].name) {
+                case classPart[0]:
+                case classPart[1]:
+                case classPart[2]:
+                case classes[j].classname:
+                case classes[j].examType.toUpperCase():
+                case classes[j].teacher.toUpperCase():
+                case classes[j].roomName.toUpperCase():
+                    document.querySelector('.' + classes[j].classname) == null && createCard(j, classes[j].classname, classPart[1] , classes[j].examType, classes[j].roomName, classes[j].teacher, classes[j].hasHDMI, classes[j].hasVGA , classes[j].dateStart, classes[j].dateEnd, classes[j].color, false);
+                    break;
             }
         }
-    }
-}
-function setParameters(){
-    parameters = true;
-    let print = "";
-    if($("#dateFrom").val() !== ""){
-        dateFrom = new Date($("#dateFrom").val());
-    }
-    if($("#dateTo").val() !== ""){
-        dateTo = new Date($("#dateTo").val());
     }
 }
 
 function search(term){
     $("#subjectInput").val("");
     term = term.toUpperCase();
-    createKeyword(keywords.length, term, "ff66aa", true);
+    createKeyword(keywords.length, term, getColor(), true);
     updateKeywords();
+}
+function showAll() {
+    for (let i = 0; i < classes.length; i++) {
+        const str = classes[i].classname.split("-");
+        const shortName = str[1].slice(0,3);
+        //For each item in classes array createCard
+        createCard(i, classes[i].classname, shortName , classes[i].examType, classes[i].roomName, classes[i].teacher, classes[i].hasHDMI, classes[i].hasVGA , classes[i].dateStart, classes[i].dateEnd, classes[i].color, true);
+    }
+}
+function clearAll() {
+    keywords.length = 0;
+    updateKeywords();
+}
+function getColor(){
+    let largest, a, b, c;do{
+        a = Math.floor(Math.random() * (255 - 110)) + 110;
+        b = Math.floor(Math.random() * (255 - 110)) + 110;
+        c = Math.floor(Math.random() * (255 - 110)) + 110;
+        largest = a;
+        if(b > largest)
+            largest = b;
+        if(c > largest)
+            largest = c;
+    }
+    while((a + b + c) < (difference * 4.1) || largest < 200);
+    return rgbToHex(a,b,c);
+}
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
--- scripts/schedule_import.js
+++ scripts/schedule_import.js
@@ -1,66 +1,98 @@
-//Format;
-//{classname, examType, dateStart, dateEnd, color}
-const classes = [{
-        classname: "SW-SDJ1-A19",
-        nameShort: "SDJ",
-        examType: "written",
-        dateStart: "2019-31-12",
-        dateEnd: "2019-02-01",
-        color: "9AF5B2"
-    },
-    {
-        classname: "SW-RWD1-A19",
-        nameShort: "RWD",
-        examType: "written",
-        dateStart: "2019-31-12",
-        dateEnd: "2019-02-01",
-        color: "FFAA96"
-    },
-    {
-        classname: "SW-SSE1-A19",
-        nameShort: "SSE",
-        examType: "written",
-        dateStart: "2019-31-12",
-        dateEnd: "2019-02-01",
-        color: "91D0EB"
-    },
-    {
-        classname: "SW-MSE1-A19",
-        nameShort: "MSE",
-        examType: "written",
-        dateStart: "2019-31-12",
-        dateEnd: "2019-02-01",
-        color: "C191EB"
-    },
-    {
-        classname: "IT-SDJ1Y-A19",
-        nameShort: "SDJ",
-        examType: "written",
-        dateStart: "2019-31-12",
-        dateEnd: "2019-02-01",
-        color: "71aaec"
-    },
-    {
-        classname: "IT-RWD1Y-A19",
-        nameShort: "RWD",
-        examType: "written",
-        dateStart: "2019-31-12",
-        dateEnd: "2019-02-01",
-        color: "7bf9d3"
-    },
-    {
-        classname: "IT-SSE1Y-A19",
-        nameShort: "SSE",
-        examType: "written",
-        dateStart: "2019-31-12",
-        dateEnd: "2019-02-01",
-        color: "d9e389"
-    },
-    {
-        classname: "IT-MSE1Y-A19",
-        nameShort: "MSE",
-        examType: "written",
-        dateStart: "2019-31-12",
-        dateEnd: "2019-02-01",
-        color: "a797d4"
-    }];
+//const classes = [
+//    {
+//        classname: "SW-SDJ1-A19",
+//        nameShort: "SDJ",
+//        examType: "written",
+//        teacher:"MIVI",
+//        roomName:"coolRoom",
+//        hasHDMI:"false",
+//        hasVGA:"true",
+//        dateStart: "2019-31-12",
+//        dateEnd: "2019-02-01",
+//        color: "9AF5B2"
+//    },
+//    {
+//        classname: "SW-RWD1-A19",
+//        nameShort: "RWD",
+//        examType: "written",
+//        teacher:"MIVI",
+//        roomName:"coolRoom",
+//        hasHDMI:"false",
+//        hasVGA:"false",
+//        dateStart: "2019-31-12",
+//        dateEnd: "2019-02-01",
+//        color: "FFAA96"
+//    },
+//    {
+//        classname: "SW-SSE1-A19",
+//        nameShort: "SSE",
+//        examType: "written",
+//        teacher:"MIVI",
+//        roomName:"coolRoom",
+//        hasHDMI:"true",
+//        hasVGA:"true",
+//        dateStart: "2019-31-12",
+//        dateEnd: "2019-02-01",
+//        color: "91D0EB"
+//    },
+//    {
+//        classname: "SW-MSE1-A19",
+//        nameShort: "MSE",
+//        examType: "written",
+//        teacher:"MIVI",
+//        roomName:"coolRoom",
+//        hasHDMI:"true",
+//        hasVGA:"false",
+//        dateStart: "2019-31-12",
+//        dateEnd: "2019-02-01",
+//        color: "C191EB"
+//    },
+//    {
+//        classname: "IT-SDJ1Y-A19",
+//        nameShort: "SDJ",
+//        examType: "written",
+//        teacher:"MIVI",
+//        roomName:"coolRoom",
+//        hasHDMI:"true",
+//        hasVGA:"true",
+//        dateStart: "2019-31-12",
+//        dateEnd: "2019-02-01",
+//        color: "71aaec"
+//    },
+//    {
+//        classname: "IT-RWD1Y-A19",
+//        nameShort: "RWD",
+//        examType: "written",
+//        teacher:"MIVI",
+//        roomName:"coolRoom",
+//        hasHDMI:"false",
+//        hasVGA:"false",
+//        dateStart: "2019-31-12",
+//        dateEnd: "2019-02-01",
+//        color: "7bf9d3"
+//    },
+//    {
+//        classname: "IT-SSE1Y-A19",
+//        nameShort: "SSE",
+//        examType: "written",
+//        teacher:"MIVI",
+//        roomName:"coolRoom",
+//        hasHDMI:"true",
+//        hasVGA:"false",
+//        dateStart: "2019-31-12",
+//        dateEnd: "2019-02-01",
+//        color: "d9e389"
+//    },
+//    {
+//        classname: "IT-MSE1Y-A19",
+//        nameShort: "MSE",
+//        examType: "written",
+//        teacher:"MIVI",
+//        roomName:"coolRoom",
+//        hasHDMI:"true",
+//        hasVGA:"true",
+//        dateStart: "2019-31-12",
+//        dateEnd: "2019-02-01",
+//        color: "a797d4"
+//    }];
+//
